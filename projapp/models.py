ce377e78db191161f39075b64d4edfb54648ee87
saov4000 <saov4000@gmail.com>
2024-04-24 06:33:02 -0300

    3 commit

--- projapp/models.py
+++ projapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Produto(models.Model):
+    descricao = models.CharField(max_length=50,blank=False,null=False)
+    preco = models.FloatField(max_length=50,blank=False,null=False)
+    estoque = models.IntegerField(max_length=50,blank=False,null=False)
+
+    def __str__(self):
+        return self.descricao
